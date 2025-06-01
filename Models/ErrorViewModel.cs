1b939d1010e7ae2ef6dc5f07f3fcbc00c325b54e
Amable Sebastian Quishpe <105821062+SebasDh56@users.noreply.github.com>
2025-05-31 22:35:49 -0500

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SistemaBoletos.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
